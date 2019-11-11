c955314487b1440033d82efac508e353235d9199
Dogway <lin_ares@hotmail.com>
2019-11-11 18:16:02 +0000

    Update Files

---
+++ MAME/All Killer, No Filler + 0.214/_classicshor.bat
@@ -0,0 +1,10 @@
+md _classicshor
+move defender.zip _classicshor
+move elevator.zip _classicshor
+move gauntlet.zip _classicshor
+move jumpbug.zip _classicshor
+move popeye.zip _classicshor
+move sfposeid.zip _classicshor
+move sharrier.zip _classicshor
+move tapper.zip _classicshor
+move pong.zip _classicshor
