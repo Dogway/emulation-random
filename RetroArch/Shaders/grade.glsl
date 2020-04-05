35f0b3884e28d09c22a9c80830ff52bf91aba80f
Dogway <lin_ares@hotmail.com>
2020-04-05 15:14:29 +0100

    grade.glsl swapped function placement

--- RetroArch/Shaders/grade.glsl
+++ RetroArch/Shaders/grade.glsl
@@ -288,6 +288,27 @@ float SatMask(float color_r, float color_g, float color_b)
 }
 
 
+float moncurve_f( float color, float gamma, float offs)
+{
+    // Forward monitor curve
+    color = clamp(color, 0.0, 1.0);
+    float fs = (( gamma - 1.0) / offs) * pow( offs * gamma / ( ( gamma - 1.0) * ( 1.0 + offs)), gamma);
+    float xb = offs / ( gamma - 1.0);
+
+    color = ( color > xb) ? pow( ( color + offs) / ( 1.0 + offs), gamma) : color * fs;
+    return color;
+}
+
+
+vec3 moncurve_f_f3( vec3 color, float gamma, float offs)
+{
+    color.r = moncurve_f( color.r, gamma, offs);
+    color.g = moncurve_f( color.g, gamma, offs);
+    color.b = moncurve_f( color.b, gamma, offs);
+    return color.rgb;
+}
+
+
 float moncurve_r( float color, float gamma, float offs)
 {
     // Reverse monitor curve
@@ -305,26 +326,6 @@ vec3 moncurve_r_f3( vec3 color, float gamma, float offs)
     color.r = moncurve_r( color.r, gamma, offs);
     color.g = moncurve_r( color.g, gamma, offs);
     color.b = moncurve_r( color.b, gamma, offs);
-    return color.rgb;
-}
-
-
-float moncurve_f( float color, float gamma, float offs)
-{
-    // Forward monitor curve
-    color = clamp(color, 0.0, 1.0);
-    float fs = (( gamma - 1.0) / offs) * pow( offs * gamma / ( ( gamma - 1.0) * ( 1.0 + offs)), gamma);
-    float xb = offs / ( gamma - 1.0);
-
-    color = ( color > xb) ? pow( ( color + offs) / ( 1.0 + offs), gamma) : color * fs;
-    return color;
-}
-
-vec3 moncurve_f_f3( vec3 color, float gamma, float offs)
-{
-    color.r = moncurve_f( color.r, gamma, offs);
-    color.g = moncurve_f( color.g, gamma, offs);
-    color.b = moncurve_f( color.b, gamma, offs);
     return color.rgb;
 }
 
