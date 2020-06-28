cb23abd40475e33823eb9e4c9be8a4c5b46f7b66
Dogway <lin_ares@hotmail.com>
2020-06-28 14:03:30 +0100

    grade - rework gamma pipeline

--- RetroArch/Shaders/grade.glsl
+++ RetroArch/Shaders/grade.glsl
@@ -819,9 +819,9 @@ void main()
          src = (signal == 0.0) ? moncurve_f_f3(src * lum_exp, 2.40, 0.055) : \
                                  moncurve_f_f3(src,           2.40, 0.055) ;
 
-    // SMPTE-C gamma at 2.222 approximates to a power law gamma of 2.0
-    vec3 gamma_fix = (gamma_type == 1.0) ? moncurve_r_f3(src, gamma_in + 0.0222, 0.099)  : \
-                                           moncurve_r_f3(src, gamma_in - 0.1222, 0.055)  ;
+// CRT Gamma: SMPTE-C gamma at 2.222 approximates to a power law gamma of 2.0
+    vec3 gamma_fix = (gamma_type == 1.0) ? moncurve_r_f3(src, gamma_in,       0.099)  : \
+                                           moncurve_r_f3(src, gamma_in + 0.2, 0.055)  ;
 
     vec3 col = gamma_fix;
 
@@ -893,11 +893,8 @@ void main()
 
 
 // OETF - Opto-Electronic Transfer Function
-    vec3 imgColor = (SPC == 3.0) ?     clamp(pow(vcolor, vec3(563./256.)),     0., 1.) : \
-                    (SPC == 2.0) ? moncurve_f_f3(vcolor,      2.20 + 0.022222, 0.0993) : \
-                    (SPC == 1.0) ?     clamp(pow(vcolor, vec3(2.20 + 0.40)),   0., 1.) : \
-                    (SPC == 0.0) ? moncurve_f_f3(vcolor,      2.20 + 0.20,     0.055)  : \
-                                       clamp(pow(vcolor, vec3(2.20 + 0.20)),   0., 1.) ;
+    vec3 imgColor = (gamma_type == 1.0) ? moncurve_f_f3(vcolor, 2.222, 0.099)  : \
+                                          moncurve_f_f3(vcolor, 2.40,  0.055)  ;
 
 
     vcolor = RGB_to_XYZ(imgColor, 0.);
@@ -1006,12 +1003,12 @@ void main()
          adjusted = clamp(XYZ_to_RGB(YxytoXYZ(vec3(base.x , adjusted.y , adjusted.z)), SPC), 0.0, 1.0);
 
 
-// EOTF - Electro-Optical Transfer Function
-    vec3 TRC = (SPC == 3.0) ?     clamp(pow(adjusted, vec3(1./(563./256.))),    0., 1.) : \
-               (SPC == 2.0) ? moncurve_r_f3(adjusted,          2.20 + 0.022222, 0.0993) : \
-               (SPC == 1.0) ?     clamp(pow(adjusted, vec3(1./(2.20 + 0.40))),  0., 1.) : \
-               (SPC == 0.0) ? moncurve_r_f3(adjusted,          2.20 + 0.20, 0.0550) : \
-                                  clamp(pow(adjusted, vec3(1./(2.20 + 0.20))),  0., 1.) ;
+// EOTF - Electro-Optical Transfer Function (Rec.709 does a Dim to Dark Surround adaptation)
+    vec3 TRC = (SPC == 3.0) ?     clamp(pow(adjusted,                  vec3(1./(563./256.))),    0., 1.) : \
+               (SPC == 2.0) ? moncurve_r_f3(adjusted,                           2.20 + 0.022222, 0.0993) : \
+               (SPC == 1.0) ?     clamp(pow(adjusted,                  vec3(1./(2.20 + 0.40))),  0., 1.) : \
+               (SPC == 0.0) ? moncurve_r_f3(adjusted,                           2.20 + 0.20,     0.0550) : \
+                              clamp(pow(pow(adjusted, vec3(1.019264)), vec3(1./(2.20 + 0.20))),  0., 1.) ;
 
 
 // Technical LUT - (in SPC space)
