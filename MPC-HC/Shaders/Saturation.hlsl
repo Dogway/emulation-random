9a2bb7d9fb2ee1ceed5f3f45245702fa8f124e09
Dogway <13509598+Dogway@users.noreply.github.com>
2023-05-25 11:54:59 +0100

    Saturation.hlsl - Masked with non-linear compensation of LED desaturation

--- MPC-HC/Shaders/Saturation.hlsl
+++ MPC-HC/Shaders/Saturation.hlsl
@@ -2,7 +2,7 @@
 /*
  * 2023 Dogway (Jose Linares)
  *
- * Simple saturation shader for Rec709 RGB
+ * Saturation shader for Rec709 RGB
  *
  */
 
@@ -11,7 +11,8 @@ sampler s0 : register(s0);
 float4 p0 :  register(c0);
 
 
-#define saturation (1.1)		 // Saturation multiplier to compensate for the Abney effect on low nits (48nits)
+#define saturation (1.3)		 // Saturation multiplier. Increase to compensate for LED saturation reduction under lower than reference white levels (ie. using 48nits instead of typical 100nits for home consumer media) (see: https://www.lightillusion.com/advanced_operation.html#oled)
+#define masked (1.0)		     // Saturation is masked via luminance, to affect more the mid to shadows areas
 
 
 static float3x3 YUV = {
@@ -31,6 +32,7 @@ float4 main(float2 tex : TEXCOORD0) : COLOR
 {
     float4 c0 = tex2D(s0, tex);
     float3x3 SYUV = {YUV[0],saturation*YUV[1],saturation*YUV[2]};
-    float3 sat = mul(c0.rgb,mul(transpose(SYUV),SRGB));
-    return float4(sat,1);
+    float3 ou = mul(c0.rgb,mul(transpose(SYUV),SRGB));
+           ou = masked==1.0 ? lerp(ou,c0.rgb,dot(c0.rgb,YUV[0])) : ou;
+    return float4(ou,1);
 }
--- MPC-HC/Shaders11/Saturation.hlsl
+++ MPC-HC/Shaders11/Saturation.hlsl
@@ -2,7 +2,7 @@
 /*
  * 2023 Dogway (Jose Linares)
  *
- * Simple saturation shader for Rec709 RGB
+ * Saturation shader for Rec709 RGB
  *
  */
 
@@ -11,7 +11,8 @@ Texture2D tex : register(t0);
 SamplerState samp : register(s0);
 
 
-#define saturation (1.1)		 // Saturation multiplier to compensate for the Abney effect on low nits (48nits)
+#define saturation (1.3)		 // Saturation multiplier. Increase to compensate for LED saturation reduction under lower than reference white levels (ie. using 48nits instead of typical 100nits for home consumer media) (see: https://www.lightillusion.com/advanced_operation.html#oled)
+#define masked (1.0)		     // Saturation is masked via luminance, to affect more the mid to shadows areas
 
 
 static float3x3 YUV = {
@@ -31,6 +32,7 @@ float4 main(float4 pos : SV_POSITION, float2 coord : TEXCOORD) : SV_Target
 {
     float4 c0 = tex.Sample(samp, coord);
     float3x3 SYUV = {YUV[0],saturation*YUV[1],saturation*YUV[2]};
-    float3 sat = mul(c0.rgb,mul(transpose(SYUV),SRGB));
-    return float4(sat,1);
+    float3 ou = mul(c0.rgb,mul(transpose(SYUV),SRGB));
+           ou = masked==1.0 ? lerp(ou,c0.rgb,dot(c0.rgb,YUV[0])) : ou;
+    return float4(ou,1);
 }
